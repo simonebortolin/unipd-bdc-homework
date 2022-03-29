e958c00c1ff8ecd2031aff5292e42d60dd7a8235
Simone Bortolin <me@simonebortolin.it>
2022-03-29 23:09:55 +0200

    Add point 3, 5, 6

--- src/main/java/GP021.java
+++ src/main/java/GP021.java
@@ -6,7 +6,10 @@ import scala.Int;
 import scala.Tuple2;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.*;
+
+
 
 public class GP021 {
 
@@ -45,7 +48,7 @@ public class GP021 {
         // SETTING GLOBAL VARIABLES
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        long numdocs, numwords;
+        long numdocs;
         numdocs = rawData.count();
         System.out.println("Number of rows = " + numdocs);
 
@@ -63,19 +66,70 @@ public class GP021 {
         System.out.println("Product-Customer Pairs = " + productCustomer.count());
 
         JavaPairRDD<String,Integer> productPopularity1 = productCustomer.flatMapToPair((it) ->
-            Collections.singletonList(new Tuple2<>(it._1, it._2)).iterator() // not working
-        ).groupByKey().flatMapToPair((it) ->
+                        Collections.singletonList(new Tuple2<>(it._1, 1)).iterator()
+                ).groupByKey().mapPartitionsToPair((it) -> {
+                    Map<String, Integer>  map = new HashMap<>();
 
-             Collections.singletonList(new Tuple2<>(it._1, 1)).iterator() // not working
-        ).reduceByKey(Integer::sum);
+                    for (; it.hasNext(); ) {
+                        Tuple2<String, Iterable<Integer>> i = it.next();
+                        int s = 0;
+                        for (int j : i._2) {
+                            s += j;
+                        }
+                        map.put(i._1, map.getOrDefault(i._1,0) + s);
+                    }
+                    List<Tuple2<String, Integer>> pairs = new ArrayList<>();
+                    for (Map.Entry<String, Integer> e : map.entrySet()) {
+                        pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
+                    }
+                    return pairs.iterator();
+                });
 
         System.out.println("productPopularity1");
+        /*List<Tuple2<String ,Integer>> output=new ArrayList<>(H);
+        for(int i=0;i<H;i++) {
+            productPopularity1.max(Comparator.comparingInt(a -> a._2));
+        }*/
+        if(H != 0) {
+            System.out.println("Top "+H+" Products and their Popularities");
+            for (Tuple2<String, Integer> item : productPopularity1.takeOrdered(H, new SerializableComparator<Tuple2<String, Integer>>() {
+                @Override
+                public int compare(Tuple2<String, Integer> o1, Tuple2<String, Integer> o2) {
+                    if(!Objects.equals(o2._2, o1._2))
+                        return o2._2 - o1._2;
+                    return o2._1.compareTo(o1._1);
+                }
+            })) {
+                System.out.print("Product: " + item._1 + " Popularity: " + item._2 + "; ");
+            }
+            System.out.println();
+        } else {
+            //productPopularity1.sortByKey();
+            for (Tuple2<String, Integer> item : productPopularity1.sortByKey().collect()) {
+                System.out.print("Product: " + item._1 + " Popularity: " + item._2 + "; ");
+            }
+            System.out.println();
+        }
 
+       /* JavaPairRDD<String,Integer> productPopularity1 = productCustomer.flatMapToPair((it) ->
+            Collections.singletonList(new Tuple2<>(it._1, it._2)).iterator() // not working
+        ).groupByKey().flatMapToPair((it) ->
+             Collections.singletonList(new Tuple2<>(it._1, 1)).iterator() // not working
+        ).reduceByKey(Integer::sum);
+        System.out.println("productPopularity1");
         for(Tuple2<String, Integer> item : productPopularity1.collect()) {
             System.out.print("Product: "+ item._1 + " Popularity: "+ item._2+",");
         }
-        System.out.println();
+        System.out.println();*/
 
+
+    }
+
+    public interface SerializableComparator<T> extends Comparator<T>, Serializable {
+
+        static <T> SerializableComparator<T> serialize(SerializableComparator<T> comparator) {
+            return comparator;
+        }
 
     }
 
