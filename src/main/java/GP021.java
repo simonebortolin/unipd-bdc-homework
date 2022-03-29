7aff13b4a40d9dd3261ebe3fe6eb50bc99f5b4f2
Simone Bortolin <me@simonebortolin.it>
2022-03-29 23:10:50 +0200

    Add point 4

--- src/main/java/GP021.java
+++ src/main/java/GP021.java
@@ -10,7 +10,6 @@ import java.io.Serializable;
 import java.util.*;
 
 
-
 public class GP021 {
 
     public static void main(String[] args) throws IOException {
@@ -45,12 +44,16 @@ public class GP021 {
         JavaRDD<String> rawData = sc.textFile(args[3]).repartition(K).cache();
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // SETTING GLOBAL VARIABLES
+        // STEP 1
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        long numdocs;
-        numdocs = rawData.count();
-        System.out.println("Number of rows = " + numdocs);
+        long rows;
+        rows = rawData.count();
+        System.out.println("Number of rows = " + rows);
+
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        // STEP 2
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
         JavaPairRDD<String, Integer> productCustomer = rawData.flatMapToPair((it) -> {
             String[] items = it.split(",");
@@ -58,71 +61,71 @@ public class GP021 {
             String theC = items[6];
             String theQ = items[3];
             String theS = items[7];
-            if(Integer.parseInt(theQ) > 0 && (S.equals("all") || S.equals(theS)))
-                return Collections.singletonList(new Tuple2<>(new Tuple2<>(theP, Integer.parseInt(theC)),items)).iterator();
+            if (Integer.parseInt(theQ) > 0 && (S.equals("all") || S.equals(theS)))
+                return Collections.singletonList(new Tuple2<>(new Tuple2<>(theP, Integer.parseInt(theC)), items)).iterator();
             return Collections.emptyListIterator();
-        }).groupByKey().flatMapToPair((it)-> Collections.singletonList(it._1).iterator());
+        }).groupByKey().flatMapToPair((it) -> Collections.singletonList(it._1).iterator());
 
         System.out.println("Product-Customer Pairs = " + productCustomer.count());
 
-        JavaPairRDD<String,Integer> productPopularity1 = productCustomer.flatMapToPair((it) ->
-                        Collections.singletonList(new Tuple2<>(it._1, 1)).iterator()
-                ).groupByKey().mapPartitionsToPair((it) -> {
-                    Map<String, Integer>  map = new HashMap<>();
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        // STEP 3
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-                    for (; it.hasNext(); ) {
-                        Tuple2<String, Iterable<Integer>> i = it.next();
-                        int s = 0;
-                        for (int j : i._2) {
-                            s += j;
-                        }
-                        map.put(i._1, map.getOrDefault(i._1,0) + s);
-                    }
-                    List<Tuple2<String, Integer>> pairs = new ArrayList<>();
-                    for (Map.Entry<String, Integer> e : map.entrySet()) {
-                        pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
-                    }
-                    return pairs.iterator();
-                });
+        JavaPairRDD<String, Integer> productPopularity1 = productCustomer.flatMapToPair((it) ->
+                Collections.singletonList(new Tuple2<>(it._1, 1)).iterator()
+        ).groupByKey().mapPartitionsToPair((it) -> {
+            Map<String, Integer> map = new HashMap<>();
+
+            while (it.hasNext()) {
+                Tuple2<String, Iterable<Integer>> i = it.next();
+                int s = 0;
+                for (int j : i._2) {
+                    s += j;
+                }
+                map.put(i._1, map.getOrDefault(i._1, 0) + s);
+            }
+            List<Tuple2<String, Integer>> pairs = new ArrayList<>();
+            for (Map.Entry<String, Integer> e : map.entrySet()) {
+                pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
+            }
+            return pairs.iterator();
+        });
 
         System.out.println("productPopularity1");
-        /*List<Tuple2<String ,Integer>> output=new ArrayList<>(H);
-        for(int i=0;i<H;i++) {
-            productPopularity1.max(Comparator.comparingInt(a -> a._2));
-        }*/
-        if(H != 0) {
-            System.out.println("Top "+H+" Products and their Popularities");
-            for (Tuple2<String, Integer> item : productPopularity1.takeOrdered(H, new SerializableComparator<Tuple2<String, Integer>>() {
-                @Override
-                public int compare(Tuple2<String, Integer> o1, Tuple2<String, Integer> o2) {
-                    if(!Objects.equals(o2._2, o1._2))
-                        return o2._2 - o1._2;
-                    return o2._1.compareTo(o1._1);
-                }
+
+        printPopularity(H, productPopularity1);
+
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        // STEP 4
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+        JavaPairRDD<String, Integer> productPopularity2 = productCustomer.mapToPair((it) ->
+                new Tuple2<>(it._1, 1)
+        ).reduceByKey(Integer::sum);
+
+        System.out.println("productPopularity2");
+
+        printPopularity(H, productPopularity2);
+    }
+
+    private static void printPopularity(int h, JavaPairRDD<String, Integer> productPopularity1) {
+        if (h != 0) {
+            System.out.println("Top " + h + " Products and their Popularities");
+            for (Tuple2<String, Integer> item : productPopularity1.takeOrdered(h, (SerializableComparator<Tuple2<String, Integer>>) (o1, o2) -> {
+                if (!Objects.equals(o2._2, o1._2))
+                    return o2._2 - o1._2;
+                return o2._1.compareTo(o1._1);
             })) {
                 System.out.print("Product: " + item._1 + " Popularity: " + item._2 + "; ");
             }
             System.out.println();
         } else {
-            //productPopularity1.sortByKey();
             for (Tuple2<String, Integer> item : productPopularity1.sortByKey().collect()) {
                 System.out.print("Product: " + item._1 + " Popularity: " + item._2 + "; ");
             }
             System.out.println();
         }
-
-       /* JavaPairRDD<String,Integer> productPopularity1 = productCustomer.flatMapToPair((it) ->
-            Collections.singletonList(new Tuple2<>(it._1, it._2)).iterator() // not working
-        ).groupByKey().flatMapToPair((it) ->
-             Collections.singletonList(new Tuple2<>(it._1, 1)).iterator() // not working
-        ).reduceByKey(Integer::sum);
-        System.out.println("productPopularity1");
-        for(Tuple2<String, Integer> item : productPopularity1.collect()) {
-            System.out.print("Product: "+ item._1 + " Popularity: "+ item._2+",");
-        }
-        System.out.println();*/
-
-
     }
 
     public interface SerializableComparator<T> extends Comparator<T>, Serializable {
